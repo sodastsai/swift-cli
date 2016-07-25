4e397a4c7a8dcf67ee489f382bdd9c7ed53c9ca6
sodastsai <sodas2002@gmail.com>
2016-07-26 01:20:35 +0800

    Refine adder sample

--- adder/main.swift
+++ adder/main.swift
@@ -1,4 +1,18 @@
-var adder = Adder(base: 5)
-adder.increase()
-adder.increase()
-print(adder)
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
+
+func main(arguments: [String]) -> Int32 {
+
+    var adder = Adder(base: 5)
+    adder.increase()
+    adder.increase()
+    print(adder)
+
+    return 0
+}
+
+let arguments = [String](Process.arguments[1..<Process.arguments.count])
+exit(main(arguments: arguments))
